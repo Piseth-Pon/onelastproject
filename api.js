0c45d46ffcfca49ddfeb3bc6228a0a31080db491
Piseth-Pon <ponpiseth8@gmail.com>
2024-12-17 19:41:18 +0700

    set up api endpoint

---
+++ api.js
@@ -0,0 +1,19 @@
+import PocketBase from './pocketbase.es.mjs';
+
+const url = 'https://onelastproject.pockethost.io/'
+const pb = new PocketBase(url)
+
+async function getAllPosts() {
+  const records = await pb.collection('posts').getFullList()
+  return records
+}
+
+async function getOnePost(id) {
+  const record = await pb.collection('posts').getOne(id)
+  return record
+}
+
+export {
+  getAllPosts,
+  getOnePost
+}
--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+import { getAllPosts, getOnePost } from "./api.js"
+
 
 window.addEventListener("DOMContentLoaded", async () => {
 
   console.log("everything is running from here")
+  const posts = await getAllPosts()
+  console.log(posts)
 
 })
